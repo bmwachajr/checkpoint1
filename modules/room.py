e99aa3eecf8c5b2f17b59fc6431bcef445c599d6
Benjamin Wacha <wacha@Benjamins-MacBook-Pro.local>
2017-06-09 19:43:37 +0300

    add module rooms

---
+++ modules/room.py
@@ -0,0 +1,12 @@
+"""
+Name        :  checkpoint1
+Author      :  Benjamin Wacha
+Github      :  @bmwachajr
+Descrption  :  Allocate rooms to new Staff and Fellows at Andela
+
+"""
+class room(object):
+    """A room at the dojo"""
+    def __init__(self, room_name, room_type):
+        self.room_name = room_name
+        self.room_type = room_type
