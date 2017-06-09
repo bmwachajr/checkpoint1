2583df262c6f9bcb07c27cd26df73d418053b6cc
Benjamin Wacha <wacha@Benjamins-MacBook-Pro.local>
2017-06-09 20:44:18 +0300

    add module dojo and class dojo

---
+++ modules/dojo.py
@@ -0,0 +1,17 @@
+"""
+Name        :  checkpoint1
+Author      :  Benjamin Wacha
+Github      :  @bmwachajr
+Descrption  :  Allocate rooms to new Staff and Fellows at Andela
+
+"""
+
+class dojo:
+    """Andela facility called the Dojo"""
+    def __init__(self):
+        self.all_offices = []
+        self.all_livingSpaces = []
+        self.avialable_rooms = []
+        self.all_fellows = []
+        self.all_staff = []
+        self.unallocated_people = []
