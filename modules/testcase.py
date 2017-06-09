c78dd3c6425ead42a80ec5b87cf4b26528c73c6e
Benjamin Wacha <wacha@Benjamins-MacBook-Pro.local>
2017-06-09 20:36:42 +0300

    add module testcase

---
+++ modules/testcase.py
@@ -0,0 +1,11 @@
+"""
+Name        :  checkpoint1
+Author      :  Benjamin Wacha
+Github      :  @bmwachajr
+Descrption  :  Allocate rooms to new Staff and Fellows at Andela
+
+"""
+
+import unittest
+from unittest import TestCase
+from person import person, fellow, staff
