0ad464bb857876d3b58757aceb65520ef3eede29
Benjamin Wacha <wacha@Benjamins-MacBook-Pro.local>
2017-06-09 20:27:11 +0300

    add fellow class to module

--- modules/person.py
+++ modules/person.py
@@ -8,7 +8,17 @@ Descrption  :  Allocate rooms to new Staff and Fellows at Andela
 
 class person:
     """Employee at andela"""
-    def __init__(self, person_name, person_type, wants_acomodation):
+    def __init__(self, person_name, person_type):
         self.person_name = person_name
         self.person_type = person_type
         self.officeSpace = 'Unallocated'
+
+class fellow(person):
+    """Fellows at the Dojo"""
+    def __init__(self, fellow_name, wants_acomodation):
+        super().__init__(fellow_name, person_type='fellow')
+
+        if wants_acomodation = "Y":
+            self.livingSpace = 'Unallocated'
+        else:
+            self.livingSpace = None
