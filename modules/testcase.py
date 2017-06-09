709ade6b32ffb34009cb2a35ab8056e59ef37331
Benjamin Wacha <wacha@Benjamins-MacBook-Pro.local>
2017-06-09 21:35:11 +0300

    add test_create_room_successfully

--- modules/testcase.py
+++ modules/testcase.py
@@ -10,3 +10,14 @@ import unittest
 from unittest import TestCase
 from dojo import dojo
 from person import person, fellow, staff
+
+class testCasesDojo(unittest.TestCase):
+    def setup(self):
+        self.dojo = dojo()
+
+    def test_create_room_successfully(self):
+        initial_room_count = len(self.dojo.all_rooms)
+        blue_office = self.dojo.creat_room("Blue", "Office")
+        self.assertTrue('blue_office')
+        new_room_count = len(self.dojo.all_rooms)
+        self.assertEqual(new_room_count - initial_room_count, 1)
