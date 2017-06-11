fe08bd915d21b64eee6f9c1c46bf381e254f6df4
Benjamin Wacha <wacha@Benjamins-MacBook-Pro.local>
2017-06-11 13:16:29 +0300

    update tests around create room living space

--- modules/testcase.py
+++ modules/testcase.py
@@ -18,9 +18,11 @@ class testCasesDojo(unittest.TestCase):
     def test_create_room_successfully(self):
         initial_room_count = len(self.dojo.all_rooms)
         blue_office = self.dojo.create_room("Blue", "Office")
+        new_livingSpace = self.dojo.create_room("St. Catherines", "livingSpace")
         self.assertTrue('blue_office')
+        self.assertTrue('new_livingSpace')
         new_room_count = len(self.dojo.all_rooms)
-        self.assertEqual(new_room_count - initial_room_count, 1)
+        self.assertEqual(new_room_count - initial_room_count, 2)
 
     def test_add_fellow_successfully(self):
         new_fellow = self.dojo.add_person("Benjamin Wacha", "Fellow", 'N')
@@ -45,14 +47,21 @@ class testCasesDojo(unittest.TestCase):
         new_fellow = self.dojo.add_person("Benjamin Wacha", "Fellow", "")
         self.assertEqual(new_staff.officeSpace, "Oculus")
         self.assertEqual(new_fellow.officeSpace, "Oculus")
+        self.assertEqual(len(new_office.occupants), 2)
+
+    def test_allocates_livingSpace_successfully(self):
+        new_office = self.dojo.create_room("Oculus", "livingspace")
+        new_fellow = self.dojo.add_person("Benjamin Wacha", "Fellow", "Y")
+        self.assertEqual(new_fellow.livingSpace, "Oculus")
 
     def test_reallocate_person_successfully(self):
-        new_office = self.dojo.create_room("Dakar", "Office")
+        self.dojo.create_room("Dakar", "Office")
         new_staff = self.dojo.add_person("Albert Emron", "Staff", "")
         self.assertEqual(new_staff.officeSpace, "Dakar")
-        other_office = self.dojo.create_room("Jinja", "Office")
+        self.dojo.create_room("Jinja", "Office")
         self.dojo.reallocate_person("Albert Emron", "Jinja")
         self.assertEqual(new_staff.officeSpace, "Jinja")
+        self.assertEqual(len(self.dojo.all_rooms), 2)
 
     def test_reallocate_Unallocated_person_successfully(self):
         new_staff = self.dojo.add_person("Albert Emron", "Staff", "")
@@ -67,9 +76,11 @@ class testCasesDojo(unittest.TestCase):
 
     def test_print_allocations(self):
         self.dojo.create_room("Dakar", "Office")
-        self.dojo.create_room("St. Catherines", "livingSpace")
+        self.dojo.create_room("St. Catherines", "livingspace")
+        self.assertTrue("St. Catherines")
         self.dojo.load_people('file.txt')
         self.assertEqual(len(self.dojo.all_employees), 7)
+        self.assertEqual(len(self.dojo.all_rooms), 2)
         self.dojo.print_allocations()
 
 if __name__ == "__main__":
